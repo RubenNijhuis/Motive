1901dd4249e16136d64e523e9ecc1d7c82104797
Ruben Nijhuis <contact@rubennijhuis.com>
2023-03-29 00:30:19 +0200

    🎉 feat: all question of the options now get asked and put in the options object

--- script.js
+++ script.js
@@ -1,46 +1,107 @@
 const PWD = process.cwd();
 
 const Mustache = require("mustache");
-const prompt = require("prompt");
+const inquirer = require("inquirer");
+
 const fs = require("fs");
 const path = require("path");
 
 const templateFolder = path.join(__dirname, "templates");
 
-let templateNames = [];
+const templateNames = fs.readdirSync(templateFolder);
 
-fs.readdir(templateFolder, (err, files) => {
-  if (err) {
-    console.log(err);
-    return;
-  }
-  files.forEach((file) => {
-    templateNames.push(file);
-  });
+const prompt = inquirer.createPromptModule();
 
-  console.log("Select a template");
-  for (const name of templateNames) {
-    console.log(name);
-  }
+let selectedTemplate = "";
+let templateOptions = {};
 
-  prompt.start();
+const getTemplateDetails = () => {
+  const selectedTemplatePath = path.join(
+    __dirname,
+    "templates",
+    selectedTemplate
+  );
 
-  const askTemplateName = () => {
-    prompt.get(["templateSelection"], (err, result) => {
-      const found = templateNames.find(
-        (name) => name === result.templateSelection
-      );
+  const optionsFile = path.join(selectedTemplatePath, "options.json");
 
-      if (!found) {
-        console.log(result.templateSelection);
-        console.log("this template doesn't exist");
-        return;
+  const templateDetails = fs.readFileSync(optionsFile, { encoding: "utf8" });
+  const unparsedOptions = JSON.parse(templateDetails);
+  const filesInTemplate = fs.readdirSync(selectedTemplatePath);
+
+  const getOtherTemplateDetails = () => {
+    let otherTemplateDetailsQuestions = [];
+
+    for (const [fileKey, _] of Object.entries(unparsedOptions)) {
+      if (fileKey === "general") continue;
+
+      for (const [fileQuestion, _] of Object.entries(
+        unparsedOptions[fileKey]
+      )) {
+        otherTemplateDetailsQuestions.push({
+          type: "input",
+          name: `${fileKey}-${fileQuestion}`,
+          message: `Please provide ${fileQuestion}:`,
+        });
       }
 
-      console.log("Selected " + found);
-    });
-  };
-  askTemplateName();
-});
+      // Ask said questions
+      prompt(otherTemplateDetailsQuestions)
+        .then((answer) => {
+          for (const [answerKey, answerValue] of Object.entries(answer)) {
+            const [answerKeyFile, answerKeyFileOption] = answerKey.split("-");
 
-// const output = Mustache.render("")
+            templateOptions[answerKeyFile][answerKeyFileOption] = answerValue;
+          }
+        })
+        .finally(() => {
+          console.log(templateOptions);
+        });
+    }
+  };
+
+  if (unparsedOptions.general) {
+    const generalOptions = { ...unparsedOptions.general };
+    let generalOptionsQuestions = [];
+
+    // Place the general keys in the template options
+    for (const fileName of filesInTemplate) {
+      if (fileName === "options.json") continue;
+        templateOptions[fileName] = { ...generalOptions };
+    }
+
+    // Create questions for each general input key
+    for (const [key, _] of Object.entries(unparsedOptions.general)) {
+      generalOptionsQuestions.push({
+        type: "input",
+        name: key,
+        message: `Please provide ${key}:`,
+      });
+    }
+
+    // Ask said questions
+    prompt(generalOptionsQuestions).then((answer) => {
+      // Add the value of each question to the corresponding key in the template options
+      for (const [templateOptionKey, _] of Object.entries(templateOptions)) {
+        for (const [answerKey, questionResponse] of Object.entries(answer)) {
+          templateOptions[templateOptionKey][answerKey] = questionResponse;
+        }
+      }
+
+      getOtherTemplateDetails();
+    });
+  }
+};
+
+const askTemplateName = () => {
+  prompt({
+    type: "list",
+    name: "selection",
+    message: "What template do you need?",
+    choices: [...templateNames],
+  }).then((answer) => {
+    selectedTemplate = answer.selection;
+    getTemplateDetails();
+  });
+};
+
+askTemplateName();
